19d9305bf4b161d3fa325131f15c96d2b4987cfa
davidedascenzoLS <davide.dascenzo@laurasessa.com>
2025-08-05 17:02:08 +0200

    Update: rimozione eshop dai componenti

--- src/components/eshop/ProductTitle.js
+++
@@ -1,8 +0,0 @@
-"use client";
-export default function Title({ title }) {
-    return (
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8 mt-32 md:mt-32 pt-24 pb-16 ">
-        <h1 className="text-3xl md:text-5xl font-thin text-gray-900">{title}</h1>
-      </div>
-    );
-  }
